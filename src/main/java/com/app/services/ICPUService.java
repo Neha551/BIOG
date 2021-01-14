de7cfce9a267d6ec2130cc54e084cce5e4ba4b47
Ronak Suchak <ronaksuchak16@hotmail.com>
2021-01-14 21:46:00 +0530

    Services and Repo for RAM,PowerSupply,MOBO,Storage

--- src/main/java/com/app/services/ICPUService.java
+++ src/main/java/com/app/services/ICPUService.java
@@ -7,9 +7,13 @@ import com.app.pojos.Cpu;
 
 public interface ICPUService {
 	List<Cpu> getAllCpu();
+
 	Cpu getCpuById(int id);
-	Cpu updateCpuById(int id,Cpu obj);
+
+	Cpu updateCpuById(int id, Cpu obj);
+
 	Cpu deleteCpuById(int id);
+
 	Cpu createCpu(Cpu obj);
 
 }
