6771bf7b67f9ce5f7bd03753ca509b8197bd28eb
Ronak Suchak <ronaksuchak16@hotmail.com>
2021-01-15 13:27:29 +0530

    rebase commit

--- src/main/java/com/app/controllers/TestController.java
+++ src/main/java/com/app/controllers/TestController.java
@@ -3,13 +3,12 @@ package com.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.pojos.core.Cpu;
-import com.app.services.ICPUService;
+import com.app.services.core.ICPUService;
 
 @RestController
 @RequestMapping("/pc")
