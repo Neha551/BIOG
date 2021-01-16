d8460ecd6d845cdefb2b63dbcb44c15b5a69847c
Neha551 <neha@LAPTOP-9GJQ2HKF.localdomain>
2021-01-16 12:15:28 +0530

    services completetd

---
+++ src/main/java/com/app/services/accessories/interfaces/ICaseAccessoryService.java
@@ -0,0 +1,22 @@
+package com.app.services.accessories.interfaces;
+
+import java.util.List;
+
+import com.app.entities.accessories.CaseAccessory;
+import com.app.entities.core.Cpu;
+import com.app.entities.core.Product;
+
+
+
+public interface ICaseAccessoryService {
+
+	List<Product> getAllCaseAccessory();
+
+	Product getCaseAccessoryById(int id);
+
+	Product updateCaseAccessoryId(CaseAccessory obj);
+
+	Product deleteCaseAccessoryId(int id);
+
+	Product createCaseAccessory(CaseAccessory obj);
+}
