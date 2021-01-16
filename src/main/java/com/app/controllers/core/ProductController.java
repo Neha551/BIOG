f19dac2b8bdb64b6356627610cf058195e89bf35
Neha551 <neha@LAPTOP-9GJQ2HKF.localdomain>
2021-01-16 14:19:35 +0530

    core getALL completed

--- src/main/java/com/app/controllers/core/ProductController.java
+++ src/main/java/com/app/controllers/core/ProductController.java
@@ -19,10 +19,16 @@ import com.app.entities.core.Product;
 import com.app.services.core.interfaces.ICPUService;
 import com.app.services.core.interfaces.ICpuCaseService;
 import com.app.services.core.interfaces.ICpuCoolerService;
+import com.app.services.core.interfaces.IExternalStorageService;
 import com.app.services.core.interfaces.IGraphicsCardService;
+import com.app.services.core.interfaces.IMonitorService;
 import com.app.services.core.interfaces.IMotherboardService;
+import com.app.services.core.interfaces.IOperatingSystemService;
+import com.app.services.core.interfaces.IOpticalDriveService;
 import com.app.services.core.interfaces.IPowerSupplyService;
 import com.app.services.core.interfaces.IRAMemoryService;
+import com.app.services.core.interfaces.ISoftwareService;
+import com.app.services.core.interfaces.IStorageService;
 import com.app.utils.ProductConstants;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -31,44 +37,271 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-
+     //*****************           CORE(13)  *********************************** 
+	
+	//1
 	@Autowired
 	private ICPUService cpuService;
+	
+	//2
 	@Autowired
 	private IRAMemoryService ramService;
+	
+	//3
 	@Autowired
-	private IPowerSupplyService psuService;
+	private IPowerSupplyService powerSupplyService;
+	
+	//4
 	@Autowired
-	private ICpuCaseService caseService;
+	private ICpuCaseService cpuCaseService;
+	
+	//5
 	@Autowired
 	private ICpuCoolerService coolerService;
+	
+	//6
 	@Autowired
 	private IGraphicsCardService gpuService;
+	
+	//7
 	@Autowired
-	private IMotherboardService moboService;
-
+	private IMotherboardService motherService;
+	
+	//8
+	@Autowired
+	private IMonitorService  monitorService;
+	
+	//9
+	@Autowired
+	private IOpticalDriveService opticalDriveService;
+	
+	//10
+	@Autowired
+	private IOperatingSystemService  operatingSystemService;
+	
+	//11
+	@Autowired
+	private ISoftwareService  softwareService;
+	
+	//12
+	@Autowired
+	private IStorageService  storageService;
+	
+	//13
+	@Autowired
+	private IExternalStorageService  externalStorageService;
+	
+	
+	
+	//************************************* ACCESSORIES (5) ***************************************
+	//************************************* NETWORKING (3) ******************************************
+	//************************************* PERIPHERALS (5) ******************************************
+	
+	
 	@GetMapping("/{type}")
-	public List<Product> showAllCpuDetails(@PathVariable String type) {
+	public ResponseEntity<?> showAllProductDetails(@PathVariable String type) {
 		switch (type) {
+		
+		//*******************************  CORE(13) *******************************************
 		case ProductConstants.CORE_CPU:
-			return cpuService.getAllCpu();
-		case ProductConstants.CORE_COOLER:
-		case ProductConstants.CORE_CASE:
-		case ProductConstants.CORE_GPU:
-		case ProductConstants.CORE_MOBO:
-		case ProductConstants.CORE_MONITOR:
-		case ProductConstants.CORE_OPTICAL:
-		case ProductConstants.CORE_PSU:
-		case ProductConstants.CORE_RAM:
-		case ProductConstants.CORE_STORAGE:
-		case ProductConstants.ACC_CASE:
-		case ProductConstants.ACC_FAN_CONTROLLER:
-		case ProductConstants.ACC_THERMAL_COMPOUND:
-		case ProductConstants.ACC_UPS:
-		case ProductConstants.NW_SOUND_CARD:
-		case ProductConstants.NW_WIRED_CARD:
-		case ProductConstants.NW_WIRELESS_CARD:
+			                                     System.out.println("in fetch all Cpu Details");
+		                                 	try{
+		                                 		List<Product> products = cpuService.getAllCpu();
+		                                 		 return new ResponseEntity<>(products, HttpStatus.OK);
+		                                 	   }
+			                    
+			                                  catch(Exception e)
+			                                  { 
+			                                	  // if (products.isEmpty())
+				                                   return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			                                  } 
+			                               
 
+		case ProductConstants.CORE_COOLER:
+			                                System.out.println("in fetch all Cpu Cooler Details");
+			                                try {
+			                                 List<Product> cooler = coolerService.getAllCpuCooler();
+			                              // non empty list
+			                                 return new ResponseEntity<>(cooler, HttpStatus.OK);
+			                                }
+		                                    	catch(Exception e) {
+		                                    		// chk if empty
+		                                    	
+			                                   // if (cooler.isEmpty())
+				                               return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		                                    	}
+			                                     
+			                                     
+		case ProductConstants.CORE_CASE:     System.out.println("in fetch cpu cases products");
+		                                          try {
+                                                  List<Product> cpuCase = cpuCaseService.getAllCpuCase();
+                                                 // non empty list
+                                                    return new ResponseEntity<>(cpuCase, HttpStatus.OK);
+                                                             }
+                                                        	catch(Exception e) {
+           		                                       // chk if empty
+           	                  
+                                                               // if (cooler.isEmpty())
+                                                      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+           	                                           }
+			                               
+		 
+		
+		
+		
+		case ProductConstants.CORE_GPU:
+			                                  System.out.println("in fetch gpu products");
+                                              try {  List<Product> gpu = gpuService.getAllGraphicsCard();
+                                             // non empty list
+                                                return new ResponseEntity<>(gpu, HttpStatus.OK);
+                                              }
+                                                 // chk if empty
+                                                catch(Exception e) {//if (gpu.isEmpty())
+                                             return new ResponseEntity<>(HttpStatus.NO_CONTENT);}
+                                        
+		case ProductConstants.CORE_MOBO:
+			                              System.out.println("in fetch motherboard products");
+                                        try { List<Product> motherboard = motherService.getAllMotherboard();
+                                         // non empty list
+                                         return new ResponseEntity<>(motherboard, HttpStatus.OK);}
+                                         
+                                          catch(Exception e) 
+                                              {// chk if empty
+                                        	  //if (motherboard.isEmpty())
+                                          return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                                          }
+                                          
+		case ProductConstants.CORE_MONITOR:
+			                               System.out.println("in fetch monitor products");
+                                        try { List<Product> monitor = monitorService.getAllMonitor();
+                                           // non empty list
+                                          return new ResponseEntity<>(monitor, HttpStatus.OK);}
+        
+                                        catch(Exception e) 
+                                           {// chk if empty
+       	                                       //if (motherboard.isEmpty())
+                                                      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                                                  }
+			                                 
+		case ProductConstants.CORE_OPTICAL:  
+			  System.out.println("in fetch optical drive products");
+              try { List<Product> optical = opticalDriveService.getAllOpticalDrive();
+                 // non empty list
+                return new ResponseEntity<>(optical, HttpStatus.OK);}
+
+              catch(Exception e) 
+                 {// chk if empty
+                        //if (motherboard.isEmpty())
+                            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                        }
+			
+		case ProductConstants.CORE_POWERSUPPLY:                   
+			  System.out.println("in fetch power supply  products");
+              try { List<Product> powerSupply = powerSupplyService.getAllPowerSupply();
+                 // non empty list
+                return new ResponseEntity<>(powerSupply, HttpStatus.OK);}
+
+              catch(Exception e) 
+                 {// chk if empty
+                        //if (motherboard.isEmpty())
+                            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                        }
+			
+		case ProductConstants.CORE_RAM:    
+			                             System.out.println("in fetch ram  products");
+                                       try { List<Product> ram = ramService.getAllRAMMemory();
+                                           // non empty list
+                                       return new ResponseEntity<>(ram, HttpStatus.OK);}
+
+                                       catch(Exception e) 
+                                     {// chk if empty
+                                        //if (motherboard.isEmpty())
+                                       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                                                }
+		
+		case ProductConstants.CORE_STORAGE:
+			
+			 System.out.println("in fetch storage products");
+             try { List<Product> storage = storageService.getAllStorage();
+                 // non empty list
+             return new ResponseEntity<>(storage, HttpStatus.OK);}
+
+             catch(Exception e) 
+           {// chk if empty
+              //if (motherboard.isEmpty())
+             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                      }
+			
+		case ProductConstants.CORE_EXTSTORAGE:
+			
+			 System.out.println("in fetch external storage products");
+             try { List<Product> externalStorage = externalStorageService.getAllExternalStorage();
+                 // non empty list
+             return new ResponseEntity<>(externalStorage, HttpStatus.OK);}
+
+             catch(Exception e) 
+           {// chk if empty
+              //if (motherboard.isEmpty())
+            	 
+             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                      }
+             
+             
+		case ProductConstants.CORE_SOFTWARE:  
+			
+			 System.out.println("in fetch software products");
+             try { List<Product> software = softwareService.getAllSoftware();
+                 // non empty list
+             return new ResponseEntity<>(software, HttpStatus.OK);}
+
+             catch(Exception e) 
+           {   // chk if empty
+              //if (motherboard.isEmpty())
+             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                      }
+             
+             
+		case ProductConstants.CORE_OS:
+			
+			 System.out.println("in fetch external storage products");
+             try { List<Product> operatingSystem = operatingSystemService.getAllOperatingSystem();
+                 // non empty list
+             return new ResponseEntity<>(operatingSystem, HttpStatus.OK);}
+
+             catch(Exception e) 
+           {   // chk if empty
+              //if (motherboard.isEmpty())
+             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                      }
+             
+             
+             
+             
+       
+             
+             
+             
+             
+             
+             
+			//************************************* ACCESSORIES (5) ***************************************
+		case ProductConstants.ACC_CASE:
+			break;
+		case ProductConstants.ACC_FAN_CONTROLLER:
+			break;
+		case ProductConstants.ACC_THERMAL_COMPOUND:
+			break;
+		case ProductConstants.ACC_UPS:
+			break;
+			
+			//************************************* NETWORKING (3) ******************************************
+		case ProductConstants.NW_SOUND_CARD:
+			break;
+		case ProductConstants.NW_WIRED_CARD:
+			break;
+		case ProductConstants.NW_WIRELESS_CARD:
+			break;
+			//************************************* PERIPHERALS (5) ******************************************
 		default:
 			break;
 		}
@@ -79,21 +312,37 @@ public class ProductController {
 	public Product showCpuDetailById(@PathVariable int id, @PathVariable String type) {
 		switch (type) {
 		case ProductConstants.CORE_CPU:
+			break;
 		case ProductConstants.CORE_COOLER:
+			break;
 		case ProductConstants.CORE_CASE:
+			break;
 		case ProductConstants.CORE_GPU:
+			break;
 		case ProductConstants.CORE_MOBO:
+			break;
 		case ProductConstants.CORE_MONITOR:
+			break;
 		case ProductConstants.CORE_OPTICAL:
-		case ProductConstants.CORE_PSU:
+			break;
+		case ProductConstants.CORE_POWERSUPPLY:
+			break;
 		case ProductConstants.CORE_RAM:
+			break;
 		case ProductConstants.CORE_STORAGE:
+			break;
 		case ProductConstants.ACC_CASE:
+			break;
 		case ProductConstants.ACC_FAN_CONTROLLER:
+			break;
 		case ProductConstants.ACC_THERMAL_COMPOUND:
+			break;
 		case ProductConstants.ACC_UPS:
+			break;
 		case ProductConstants.NW_SOUND_CARD:
+			break;
 		case ProductConstants.NW_WIRED_CARD:
+			break;
 		case ProductConstants.NW_WIRELESS_CARD:
 
 		default:
@@ -108,22 +357,39 @@ public class ProductController {
 	public Product deleteCpuDetailById(@PathVariable int id, @PathVariable String type) {
 		switch (type) {
 		case ProductConstants.CORE_CPU:
+			break;
 		case ProductConstants.CORE_COOLER:
+			break;
 		case ProductConstants.CORE_CASE:
+			break;
 		case ProductConstants.CORE_GPU:
+			break;
 		case ProductConstants.CORE_MOBO:
+			break;
 		case ProductConstants.CORE_MONITOR:
+			break;
 		case ProductConstants.CORE_OPTICAL:
-		case ProductConstants.CORE_PSU:
+			break;
+		case ProductConstants.CORE_POWERSUPPLY:
+			break;
 		case ProductConstants.CORE_RAM:
+			break;
 		case ProductConstants.CORE_STORAGE:
+			break;
 		case ProductConstants.ACC_CASE:
+			break;
 		case ProductConstants.ACC_FAN_CONTROLLER:
+			break;
 		case ProductConstants.ACC_THERMAL_COMPOUND:
+			break;
 		case ProductConstants.ACC_UPS:
+			break;
 		case ProductConstants.NW_SOUND_CARD:
+			break;
 		case ProductConstants.NW_WIRED_CARD:
+			break;
 		case ProductConstants.NW_WIRELESS_CARD:
+			break;
 
 		default:
 			break;
@@ -142,22 +408,39 @@ public class ProductController {
 	public Product createProduct(@RequestBody String product, @PathVariable String type) {
 		switch (type) {
 		case ProductConstants.CORE_CPU:
+			break;
 		case ProductConstants.CORE_COOLER:
+			break;
 		case ProductConstants.CORE_CASE:
+			break;
 		case ProductConstants.CORE_GPU:
+			break;
 		case ProductConstants.CORE_MOBO:
+			break;
 		case ProductConstants.CORE_MONITOR:
+			break;
 		case ProductConstants.CORE_OPTICAL:
-		case ProductConstants.CORE_PSU:
+			break;
+		case ProductConstants.CORE_POWERSUPPLY:
+			break;
 		case ProductConstants.CORE_RAM:
+			break;
 		case ProductConstants.CORE_STORAGE:
+			break;
 		case ProductConstants.ACC_CASE:
+			break;
 		case ProductConstants.ACC_FAN_CONTROLLER:
+			break;
 		case ProductConstants.ACC_THERMAL_COMPOUND:
+			break;
 		case ProductConstants.ACC_UPS:
+			break;
 		case ProductConstants.NW_SOUND_CARD:
+			break;
 		case ProductConstants.NW_WIRED_CARD:
+			break;
 		case ProductConstants.NW_WIRELESS_CARD:
+			break;
 
 		default:
 			break;
@@ -181,23 +464,39 @@ public class ProductController {
 	public ResponseEntity<?> updateProduct(@RequestBody Cpu product, @PathVariable String type, @PathVariable int id) {
 		switch (type) {
 		case ProductConstants.CORE_CPU:
-			return null;
+			break;
 		case ProductConstants.CORE_COOLER:
+			break;
 		case ProductConstants.CORE_CASE:
+			break;
 		case ProductConstants.CORE_GPU:
+			break;
 		case ProductConstants.CORE_MOBO:
+			break;
 		case ProductConstants.CORE_MONITOR:
+			break;
 		case ProductConstants.CORE_OPTICAL:
-		case ProductConstants.CORE_PSU:
+			break;
+		case ProductConstants.CORE_POWERSUPPLY:
+			break;
 		case ProductConstants.CORE_RAM:
+			break;
 		case ProductConstants.CORE_STORAGE:
+			break;
 		case ProductConstants.ACC_CASE:
+			break;
 		case ProductConstants.ACC_FAN_CONTROLLER:
+			break;
 		case ProductConstants.ACC_THERMAL_COMPOUND:
+			break;
 		case ProductConstants.ACC_UPS:
+			break;
 		case ProductConstants.NW_SOUND_CARD:
+			break;
 		case ProductConstants.NW_WIRED_CARD:
+			break;
 		case ProductConstants.NW_WIRELESS_CARD:
+			break;
 
 		default:
 			break;
--- src/main/java/com/app/utils/ProductConstants.java
+++ src/main/java/com/app/utils/ProductConstants.java
@@ -3,7 +3,7 @@ package com.app.utils;
 public class ProductConstants {
 
 	 
-//	CORE COMPONENTS
+//	CORE COMPONENTS(13)
 	public static final String CORE_CPU="cpu";
 	 public static final String CORE_RAM="ram";
 	 public static final String CORE_MOBO="mother-board";
@@ -12,10 +12,13 @@ public class ProductConstants {
 	 public static final String CORE_GPU="gpu";
 	 public static final String CORE_MONITOR="monitor";
 	 public static final String CORE_OPTICAL="optical";
-	 public static final String CORE_PSU="psu";
+	 public static final String CORE_OS="os";
 	 public static final String CORE_CASE="CASE";
+	 public static final String CORE_SOFTWARE="software";
+	 public static final String CORE_POWERSUPPLY="power-supply";
+	 public static final String CORE_EXTSTORAGE="external-storage";
 	 
-//	 ACCESSORIES 
+//	 ACCESSORIES(5)
 	 
 	 public static final String ACC_CASE="CASE-ACC";
 	 public static final String ACC_CASE_FAN="CASE-FAN";
@@ -23,10 +26,17 @@ public class ProductConstants {
 	 public static final String ACC_THERMAL_COMPOUND="THERMAL-COMP-ACC";
 	 public static final String ACC_UPS="UPS-ACC";
 	 
-//NETWORKING
+//NETWORKING(3)
 	 
 	 public static final String NW_SOUND_CARD="SOUND-NW";
 	 public static final String NW_WIRED_CARD="WIRED-NW";
 	 public static final String NW_WIRELESS_CARD="WIRELESS-NW";
+	 
+//PERIPHERALS(5)
+	 public static final String PERI_HEADPHONES="headphone";
+	 public static final String PERI_KEYBOARD="keyboard";
+	 public static final String PERI_MICE="mice";
+	 public static final String PERI_SPEAKER="speaker";
+	 public static final String PERI_WEBCAM="webcam";
 	 
 }
